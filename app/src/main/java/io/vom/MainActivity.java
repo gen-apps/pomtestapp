72138c625be87b2817ec561984b297b1abb4eecc
geno <genad.papashvili@yahoo.com>
2022-07-15 00:08:16 +0400

    update

--- app/src/main/java/io/vom/MainActivity.java
+++ app/src/main/java/io/vom/MainActivity.java
@@ -1,13 +1,26 @@
 package io.vom;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton =  findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setClass(getApplicationContext(), HomeActivity.class);
+
+            startActivity(intent);
+        });
     }
 }
